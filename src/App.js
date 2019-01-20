125ed82b11d0caeec3fd70e319c6dd2c0db70ed8
defakt <tech@defakt.net>
2019-01-20 13:17:33 +0900

    create class components and fucntional components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-	render() {
-		return (
-			<React.Fragment>
-				<label htmlFor="bar">bar</label>
-				<input
-					type="text"
-					onChange={() => {
-						console.log('I am clicked!');
-					}}
-				/>
-			</React.Fragment>
-		);
-	}
-}
+//クラスコンポーネント
+// class App extends Component {
+// 	render() {
+// 		return (
+// 			<React.Fragment>
+// 				<label htmlFor="bar">bar</label>
+// 				<input
+// 					type="text"
+// 					onChange={() => {
+// 						console.log('I am clicked!');
+// 					}}
+// 				/>
+// 			</React.Fragment>
+// 		);
+// 	}
+// }
+
+//関数型コンポーネント
+const App = () => {
+	return (
+		<div>
+			<Cat />
+			<Cat />
+			<Cat />
+			<Cat />
+		</div>
+	);
+};
+
+const Cat = () => {
+	return <div>Meow!</div>;
+};
 
 export default App;
