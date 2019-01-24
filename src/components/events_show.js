322d2ffa08aa1e953515746e8d8d7d1c8fa33592
defakt <tech@defakt.net>
2019-01-24 09:02:56 +0900

    implement delete feature

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Field, reduxForm } from 'redux-form';
-import { postEvent } from '../actions';
+import { getEvent, deleteEvent, putEvent } from '../actions';
 
 //関数型コンポーネント
 
-class EventsNew extends Component {
+class EventsShow extends Component {
 	constructor(props) {
+		// console.log('EventsShow');
 		super(props);
 		this.onSubmit = this.onSubmit.bind(this);
+		this.onDeleteClick = this.onDeleteClick.bind(this);
+	}
+
+	componentDidMount() {
+		const { id } = this.props.match.params;
+		if (id) this.props.getEvent(id);
 	}
 
 	renderField(field) {
@@ -22,14 +29,20 @@ class EventsNew extends Component {
 		);
 	}
 
+	async onDeleteClick() {
+		const id = this.props.match.params.id;
+		await this.props.deleteEvent(id);
+		this.props.history.push('/');
+	}
+
 	async onSubmit(values) {
-		await this.props.postEvent(values);
+		await this.props.putEvent(values);
 		this.props.history.push('/');
 	}
 
 	render() {
 		const { handleSubmit, pristine, submitting, invalid } = this.props;
-		// console.log(submitting);
+		//console.log(submitting);
 
 		return (
 			<form onSubmit={handleSubmit(this.onSubmit)}>
@@ -43,6 +56,9 @@ class EventsNew extends Component {
 					<input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
 				</div>
 				<Link to="/">Cancel</Link>
+				<Link to="/" onClick={this.onDeleteClick}>
+					Delete
+				</Link>
 			</form>
 		);
 	}
@@ -54,6 +70,11 @@ const validate = (values) => {
 	if (!values.body) errors.body = 'Enter a body, please';
 	return errors;
 };
-const mapDispatchToProps = { postEvent };
 
-export default connect(null, mapDispatchToProps)(reduxForm({ validate, form: 'eventNewForm' })(EventsNew));
+const mapStateToProps = (state, ownProps) => {
+	const event = state.events[ownProps.match.params.id];
+	return { initialValues: event, event };
+};
+const mapDispatchToProps = { deleteEvent, getEvent, putEvent };
+
+export default connect(mapStateToProps, mapDispatchToProps)(reduxForm({ validate, form: 'eventShowForm', enableReinitialize: true })(EventsShow));
